e573c72c7c137d23982a002104403e72bef0f93b
devMizzen <66153273+devMizzen@users.noreply.github.com>
2020-05-30 11:28:20 +0530

    Update index.js
    Fixing $port - I

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const Enmap = require("enmap");
 const bot = new Discord.Client();
 const config = bot.config = require("./config.json");
 
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 bot.commands = new Enmap();
 bot.functions = new Enmap();
 bot.events = new Enmap();
