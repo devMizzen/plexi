176f635abd1557e06ea3eb9748c3945241f82054
cracraftyfty <40735864+cracraftyfty@users.noreply.github.com>
2020-05-31 16:33:03 +0530

    avatar command

---
+++ commands/av.js
@@ -0,0 +1,9 @@
+const Discord = require("discord.js");
+exports.run = (args, message, bot, cmds) => {
+    const user = message.mentions.users.first() || message.author;
+    const avatarEmbed = new Discord.RichEmbed()
+        .setColor(0x333333)
+        .setAuthor(user.username)
+        .setImage(user.avatarURL);
+    message.channel.send(avatarEmbed);
+}
