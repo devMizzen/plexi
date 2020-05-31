e488a619498e2ed4eda36c6d6054b9e60fabf458
devMizzen <66153273+devMizzen@users.noreply.github.com>
2020-05-31 18:19:32 +0530

    Update inventory.py

--- commands/inventory.py
+++ commands/inventory.py
@@ -7,6 +7,13 @@ from pymongo import MongoClient as mongo
 
 id = str(sys.argv[1])
 
+cluster = mongo(os.environ["MONGO_URL"])  #Same as process.env.MONGO_URL
+db = cluster.plexi_users 
+player = db.id
+dependancies = db.Dependancies
+
+inventory = {}
+
 result = 0
 print(result)
 sys.stdout.flush()
