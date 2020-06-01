a8f82a23c265fd03b3249d803ea8fded37c2ee49
devMizzen <66153273+devMizzen@users.noreply.github.com>
2020-06-01 07:24:25 +0530

    Update inventory.js

--- commands/inventory.js
+++ commands/inventory.js
@@ -8,12 +8,11 @@ const fs = require('fs');
 exports.run = (args, message, bot, cmds, cluster) => {
     const userid = `${message.author.id}`;
 
-   
     cluster.connect(err => {
-    const db = cluster.db("plexi_users")
-    const player = db.collection(userid)
+    const db = cluster.db('plexi_users')
+    const player = db.collection(`${userid}`)
     const inventory = player.findOne(
-        {_id: 'inventory'}
+        {"_id": 'inventory'}
         
     )
     console.log('Connected... Transmission Successful!')
