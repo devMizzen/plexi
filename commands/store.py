2f5a1dc8e59de3afb54747261c3c2157f3a4c1e3
devMizzen <66153273+devMizzen@users.noreply.github.com>
2020-06-03 11:54:21 +0530

    f

--- commands/store.py
+++ commands/store.py
@@ -14,9 +14,12 @@ from pymongo import MongoClient as mongo
 id = sys.argv[1]
 
 global cluster = mongo(os.environ["MONGO_URL"])  #Same as process.env.MONGO_URL
-global db = cluster.plexi_users 
-global player = db.id
-global dependancies = db.Dependancies
+global db = cluster["Containers"] 
+global player = db["Inventories"]
+
+global dpd = cluster["Dependancies"]
+global dependancies = dpd["Database"]
+global injectors = dpd["Injectors"]
 
 global numOfEntities = len(dependancies.find_one({"_id": "entityIDs"},))
 
@@ -24,8 +27,15 @@ global numOfEntities = len(dependancies.find_one({"_id": "entityIDs"},))
 #						Functions						  #
 #---------------------------------------------------------#
 
+def getSize(item):
+	itemSizes = dependancies.find_one(
+		{"_id": "ItemSizes"}
+		projection={item: True}
+	) 
+	return itemSizes
+
 def getQty(item):
-	playerInv = player.find_one({"_id": "inventory"})
+	playerInv = player.find_one({"_id": id})
 	invQty = 0
 	for slot in playerInv:
 		if slot == "isEmpty":
@@ -39,48 +49,95 @@ def getQty(item):
 			invQty += int(itemQty)
 	return(invQty)
 	
-def setQty(item, qty)
-	playerInv = player.find_one({"_id": "inventory"})
-	locs = []
-	while qty > 0
-		for slot in playerInv:		#For each slot in inventory
-			if slot == "isEmpty":
-				continue
-			itemQty = ''
-			if item in playerInv[slot]:	
-				locs.append(slot)
-				for ch in playerInv[slot]: #for each character in the value
-					if ch == 'x' or ch == ':
-						break
-					itemQty += ch
-				itemQty = int(itemQty)
-				
-				if itemQty+qty <= 64:
-					playerInv[slot] = str(itemQty+qty) + "x " + item
-					qty = 0
-					
-			elif qty!= 0:
-				for slot in playerInv:
-					if slot == "isEmpty":
-						continue
-					if playerInv[slot] == None:
-						break
-				emptySlot = slot
-				if itemQty+qty <= 64:
-					player.update_one(
-						{"_id" : "inventory"},
-						"$set" {
-							emptySlot: str(itemQty+qty) + "x " + item
-						}
-					)
-				else
-					
-			
-			
-			
-				
-				
+def setQty(item, method, invQty, qty):
 
+	playerInv.find_one(
+		{"_id": id},
+		projection={
+			"_id": False,
+			"isEmpty": False
+		}
+	)
+	if method == "sub":
+		while qty != 0:
+			for slot in playerInv:
+				if item in playerInv[slot] and qty != 0:
+					itemQty = ''
+					for ch in playerInv[slot]:
+						if ch == 'x':
+							break
+						itemQty += ch
+					itemQty = int(itemQty)
+					if itemQty == 64:
+						continue
+					if itemQty-qty in range(1, 65):
+						player.update_one(
+							{"_id": id},
+							"$set": {slot: str(itemQty-qty)+"x "+item}}
+						)
+						qty = 0
+						break
+					
+					elif itemQty-qty = 0:
+						player.update_one(
+							{"_id": id},
+							"$set": {slot: None}	
+						)
+						qty = 0
+						break
+					
+					else:
+						player.update_one(
+							{"_id": id},
+							"$set": {slot: None}
+						)
+						qty -= (64-itemQty)
+	
+	elif method == 'add':
+#		while qty != 0:
+			for slot in playerInv:
+				if item in playerInv[slot] and qty != 0:
+					itemQty = ''
+					for ch in playerInv[slot]:
+						if ch == 'x':
+							break
+						itemQty += ch
+					itemQty = int(itemQty)
+					if itemQty == 64:
+						continue
+					if itemQty+qty <= 64:
+						player.update_one(
+							{"_id": id},
+							"$set": {slot: str(itemQty+qty)+"x "+item}}
+						)
+						qty = 0
+						break
+					
+					else:
+						player.update_one(
+							{"_id": id},
+							"$set": {slot: "64x "+item}	
+						)
+						qty -= (64-itemQty)
+					
+			while qty != 0:
+				for slot in playerInv and qty != 0:
+					if playerInv[slot] == None:
+						if qty <= 64:
+							player.update_one(
+								{"_id": id},
+								"$set": {slot: str(qty)+"x "+item}
+							)
+							qty = 0
+							break
+							
+						else:
+							player.update_one(
+								{"_id": id},
+								"$set": {slot: "64x "+item}
+							)
+							qty -= 64
+							
 def price(item):
 	itemPrices = dependancies.find_one({"_id": "itemPrices"})
 	itemPrice = int(itemPrices[item])
@@ -94,115 +151,66 @@ def inventory(method, itemName, *arg):
 		else:
 			return False
 
-		'''filename = "./commands/Database/"+id+".xlsx"
-		wb = openpyxl.load_Workbook(filename)
-		ws = wb[Inventory]
-		
-		for row in range(1,int(numOfEntities)):
-			currentItem = "A"+str(row)
-			itemQty = "B"+str(row)
-			if ws[currentItem].value == itemName:
-				if int(ws[itemQty].value) >= int(qty):
-					return True
-		return False'''
-		
 	elif method == "sub":
-		SubQty = qty
-		playerInv = player.find_one({"_id": "inventory"})
+		SubQty = args[0]
 		invQty = getQty(itemName)
+		setQty(itemName, method, invQty, SubQty)
 		
-		'''filename = "./commands/Database/"+id+".xlsx"
-		wb = openpyxl.load_Workbook(filename)
-		ws = wb[Inventory]
-		
-		for row in range(1,int(numOfEntities)):
-			currentItem = "A"+str(row)
-			itemQty = "B"+str(row)
-			if ws[currentItem].value == itemName:
-				if qty < ws[itemQty]:
-					ws[itemQty] -= SubQty
-					wb.save(filename)
-					return True
-				elif qty == ws[itemQty]:
-					ws[itemQty] = None
-					ws[currentItem] = None
-				elif qty > ws[itemQty]:
-					ws[itemQty] = None
-					ws[currentItem] = None
-					SubQty -= itemQty'''
-
 	elif method == 'add':
-		addQty = qty
-		filename = "./commands/Database/"+id+".xlsx"
-		wb = openpyxl.load_Workbook(filename)
-		ws = wb[Inventory]
-		
-		for row in range(1,int(numOfEntities)):
-			currentItem = "A"+str(row)
-			itemQty = "B"+str(row)
-			if ws[currentItem].value == itemName:
-				if ws[itemQty].value + (addQty*size) <= 64:
-					ws[itemQty] += addQty*size[0]
-					wb.save(filename)
-					return True
-				
-				elif ws[itemQty].value + (addQty*size) > 64:
-					ws[itemQty] = 64
-					addQty = ws[itemQty].value + (addQty*size) - 64
-		return False
+		addQty = args[0]
+		invQty = getQty(itemName)
+		setQty(itemName, method, invQty, addQty)
 		
 	else:
-		reqSpace = qty
-		filename = "./commands/Database/"+id+".xlsx"
-		wb = openpyxl.load_Workbook(filename)
-		ws = wb[Inventory]
+		item = itemName
+		reqSpace = args[0]
+		size = args[1]
 		
-		for row in range(1,int(numOfEntities)):
-			itemQty = "B"+str(row)
-			space = (64*33+4) - reqSpace*size
-			if space < 0:
-				return False:
-			else:
-				return True
+		invQty = getQty(item)
+		if invQty >= reqSpace*size:
+			return True
+		else:
+			return False
 	
 def balance(id, method, amnt):
-	fileName = "./commands/Database/"+id+".xlsx"
-	wb = openpyxl.load_Workbook(fileName)
-	ws = wb[balance]
+	economy = cluster["Economy"]
+	bal = economy["Balance"]
 	
 	if method == "chk":
-		return ws["A1"].value
+		playerBal = bal.find_one(
+			{"_id": id},
+			projection={money: True}
+		)
+		return playerBal
 	
 	elif method == "add":
-		try:
-			ws["A1"] = ws["A1"].value + amnt
-			wb.save(fileName)
-			return True
-		except:
-			return False
+		crntBal = bal.find_one(
+			{"_id": id},
+			projection = {money: True}
+		)
+		bal.update_one(
+			{"_id": id},
+			"$set": {money: crntBal+amnt}
+		)
 	
 	elif method == 'sub':
-		try:
-			ws["A1"] = ws["A1"].value - amnt
-			wb.save(fileName)
-			return True
-		except:
-			return False
-
-def raiseError(errorCode):
-	print(errorCode)
-	sys.stdout.flush()
-
+		crntBal = bal.find_one(
+			{"_id": id},
+			projection = {money: True}
+		)
+		bal.update_one(
+			{"_id": id},
+			"$set": {money: crntBal-amnt}
+		)
 #---------------------------------------------------------#
 #						  Main  						  #
 #---------------------------------------------------------#
 
-if argv[2].lower() != "price":
-	item = sys.argv[3]
+item = sys.argv[3]
+if sys.argv[2].lower() != "price":
 	qty = sys.argv[4]
-	size = sys.argv[5]
-else:
-	item = argv[4]
+	
+	size = getSize(item)
 
 if sys.argv[1].lower() == 'sell':	
 	itemPrice = price(item)
@@ -211,26 +219,37 @@ if sys.argv[1].lower() == 'sell':
 		balance(id, "add", Credit)
 		inventory(id, "sub", itemName, qty)
 	else:
-		errorCode = "Not enough items in inventory."
-		raiseError(errorCode)
+		injectors.insert_one(
+			{"_id": "ErrorCode",
+			"Error": "Not enough items in inventory"}
+		)
 	
 elif sys.argv[1].lower() == 'buy':
 	itemPrice = price(item)
 	Credit = itemPrice*qty
 	
 	bal = balance(id, "chk", Credit)
-	inv = inventory(id, "space", qty, size)
+	inv = inventory(id, "space", item, qty, size)
 	if bal == True and inv == True:
 		balance(id, "sub", Credit)
 		inventory(id, "add", itemName, qty, size)
 	
 	elif bal == True and inv == False:
-		raiseError("Not enough space in inventory.")
+		injectors.insert_one(
+			{"_id": "ErrorCode",
+			"Error": "Not enough space in inventory"}
+		)
 		
 	else:
-		raiseError("Not enough balance to make the purchase.")
+		injectors.insert_one(
+			{"_id": "ErrorCode",
+			"Error": "Not enough balance to make the purchase"},
+		)
 		
 elif sys.argv[1].lower() == "price":
 	itemPrice = price(item)
-	result = itemPrice
-	sys.stdout.flush()
+	injectors.insert_one(
+		{"_id": "Result", 
+		"Output": itemPrice}
+	)
+	
