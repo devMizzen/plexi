e444b01f51ff254adee1060286670ef71065b65e
devMizzen <66153273+devMizzen@users.noreply.github.com>
2020-06-03 11:57:12 +0530

    Update inventory.js

--- commands/inventory.js
+++ commands/inventory.js
@@ -28,7 +28,7 @@ exports.run = (args, message, bot, cmds, cluster) => {
         console.log(Object.values(userList));*/
 
         console.log(inventory);
-        console.log(inventory._doc.isEmpty)
+        console.log(inventory[0]isEmpty)
 
         const inventoryembed = new Discord.RichEmbed()
         .setTitle(`${message.author.username}'s Inventory`)
