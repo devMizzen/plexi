398a48611f4f494b70c425ddf94faacc96d0159f
devMizzen <66153273+devMizzen@users.noreply.github.com>
2020-06-05 11:32:55 +0530

    Take III

--- commands/inventory.js
+++ commands/inventory.js
@@ -15,7 +15,7 @@ exports.run = (args, message, bot, cmds, cluster) => {
             const datb = cluster.db("Containers");
             const inventories = datb.collection("Inventories")
 
-            inventories.find({}).toArray().then((docs) => {
+            inventories.findOne({"_id": "userid"}).toArray().then((docs) => {
                 console.log('Available Documents:');
                 docs.forEach((doc, idx, array) => { 
                     const isEmpty = doc.isEmpty;
@@ -25,6 +25,58 @@ exports.run = (args, message, bot, cmds, cluster) => {
                         .setDescription("Your inventory is empty.")
                         .setFooter(`• Requested By: ${message.author.tag}`, message.author.avatarURL)
                         message.author.send(inventoryembed1);
+                    }
+                    else{
+                        const inventoryembed1 = new Discord.RichEmbed()
+                        .setTitle(`${message.author.username}'s Inventory`)
+                        .setDescription("Contents of your inventory.")
+                        .addFields(
+                            {name: "Left Hand", value : doc.lh},
+                            {name: "Slot 1", value : doc.slot1},
+                            {name: "Slot 2", value : doc.slot2},
+                            {name: "Slot 3", value : doc.slot3},
+                            {name: "Slot 4", value : doc.slot4},
+                            {name: "Slot 5", value : doc.slot5},
+                            {name: "Slot 6", value : doc.slot6},
+                            {name: "Slot 7", value : doc.slot7},
+                            {name: "Slot 8", value : doc.slot8},
+                            {name: "Slot 9", value : doc.slot9},
+                            {name: "Slot 10", value : doc.slot10},
+                            {name: "Slot 11", value : doc.slot11},
+                            {name: "Slot 12", value : doc.slot12},
+                            {name: "Slot 13", value : doc.slot13},
+                            {name: "Slot 14", value : doc.slot14},
+                            {name: "Slot 15", value : doc.slot15},
+                            {name: "Slot 16", value : doc.slot16},
+                            {name: "Slot 17", value : doc.slot17}
+                        )
+                        .setFooter(`• Requested By: ${message.author.tag}`, message.author.avatarURL)
+
+                        const invEmbed2 = new Discord.RichEmbed()
+                        .addFields(
+                            {name: "Slot 18", value : doc.slot18},
+                            {name: "Slot 19", value : doc.slot19},
+                            {name: "Slot 20", value : doc.slot20},
+                            {name: "Slot 21", value : doc.slot21},
+                            {name: "Slot 22", value : doc.slot22},
+                            {name: "Slot 23", value : doc.slot23},
+                            {name: "Slot 24", value : doc.slot24},
+                            {name: "Slot 25", value : doc.slot25},
+                            {name: "Slot 26", value : doc.slot26},
+                            {name: "Slot 27", value : doc.slot27},
+                            {name: "Slot 28", value : doc.slot28},
+                            {name: "Slot 29", value : doc.slot29},
+                            {name: "Slot 30", value : doc.slot30},
+                            {name: "Slot 31", value : doc.slot31},
+                            {name: "Slot 32", value : doc.slot32},
+                            {name: "Head", value: doc.head},
+                            {name: "Chest", value: doc.chest},
+                            {name: "Torse", value: doc.torso},
+                            {name: "Shoes", value: doc.shoe}
+                        )
+                        .setFooter(`• Requested By: ${message.author.tag}`, message.author.avatarURL)
+                        message.author.send(inventoryembed1);
+                        message.author.send(invEmbed2);
                     }
             }).catch((err) => {
                 console.log(err);
