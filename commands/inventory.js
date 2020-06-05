b01a28e27d5a5a8f895f0f98fdde3a6e65004e9e
devMizzen <66153273+devMizzen@users.noreply.github.com>
2020-06-05 12:23:48 +0530

    Update inventory.js

--- commands/inventory.js
+++ commands/inventory.js
@@ -28,53 +28,50 @@ exports.run = (args, message, bot, cmds, cluster) => {
                         message.author.send(inventoryembed1);
                     }
                     else{
-                        const inventoryembed1 = new Discord.MessageEmbed()
+                        const inventoryembed1 = new Discord.RichEmbed()
                         .setTitle(`${message.author.username}'s Inventory`)
                         .setDescription("Contents of your inventory.")
-                        .addFields(
-                            {name: "Left Hand", value : doc.lh},
-                            {name: "Slot 1", value : doc.slot1},
-                            {name: "Slot 2", value : doc.slot2},
-                            {name: "Slot 3", value : doc.slot3},
-                            {name: "Slot 4", value : doc.slot4},
-                            {name: "Slot 5", value : doc.slot5},
-                            {name: "Slot 6", value : doc.slot6},
-                            {name: "Slot 7", value : doc.slot7},
-                            {name: "Slot 8", value : doc.slot8},
-                            {name: "Slot 9", value : doc.slot9},
-                            {name: "Slot 10", value : doc.slot10},
-                            {name: "Slot 11", value : doc.slot11},
-                            {name: "Slot 12", value : doc.slot12},
-                            {name: "Slot 13", value : doc.slot13},
-                            {name: "Slot 14", value : doc.slot14},
-                            {name: "Slot 15", value : doc.slot15},
-                            {name: "Slot 16", value : doc.slot16},
-                            {name: "Slot 17", value : doc.slot17}
-                        )
+                            .addField("Left Hand", doc.lh)
+                            .addField("Slot 1", doc.slot1)
+                            .addField("Slot 2", doc.slot2)
+                            .addField("Slot 3", doc.slot3)
+                            .addField("Slot 4", doc.slot4)
+                            .addField("Slot 5", doc.slot5)
+                            .addField("Slot 6", doc.slot6)
+                            .addField("Slot 7", doc.slot7)
+                            .addField("Slot 8", doc.slot8)
+                            .addField("Slot 9", doc.slot9)
+                            .addField("Slot 10", doc.slot10)
+                            .addField("Slot 11", doc.slot11)
+                            .addField("Slot 12", doc.slot12)
+                            .addField("Slot 13", doc.slot13)
+                            .addField("Slot 14", doc.slot14)
+                            .addField("Slot 15", doc.slot15)
+                            .addField("Slot 16", doc.slot16)
+                            .addField("Slot 17", doc.slot17)
                         .setFooter(`• Requested By: ${message.author.tag}`, message.author.avatarURL)
 
-                        const invEmbed2 = new Discord.MessageEmbed()
-                        .addFields(
-                            {name: "Slot 18", value : doc.slot18},
-                            {name: "Slot 19", value : doc.slot19},
-                            {name: "Slot 20", value : doc.slot20},
-                            {name: "Slot 21", value : doc.slot21},
-                            {name: "Slot 22", value : doc.slot22},
-                            {name: "Slot 23", value : doc.slot23},
-                            {name: "Slot 24", value : doc.slot24},
-                            {name: "Slot 25", value : doc.slot25},
-                            {name: "Slot 26", value : doc.slot26},
-                            {name: "Slot 27", value : doc.slot27},
-                            {name: "Slot 28", value : doc.slot28},
-                            {name: "Slot 29", value : doc.slot29},
-                            {name: "Slot 30", value : doc.slot30},
-                            {name: "Slot 31", value : doc.slot31},
-                            {name: "Slot 32", value : doc.slot32},
-                            {name: "Head", value: doc.head},
-                            {name: "Chest", value: doc.chest},
-                            {name: "Torse", value: doc.torso},
-                            {name: "Shoes", value: doc.shoe}
-                        )
+                        const invEmbed2 = new Discord.RichEmbed()
+                            .addField("Slot 18", doc.slot18)
+                            .addField("Slot 19", doc.slot19)
+                            .addField("Slot 20", doc.slot20)
+                            .addField("Slot 21", doc.slot21)
+                            .addField("Slot 22", doc.slot22)
+                            .addField("Slot 23", doc.slot23)
+                            .addField("Slot 24", doc.slot24)
+                            .addField("Slot 25", doc.slot25)
+                            .addField("Slot 26", doc.slot26)
+                            .addField("Slot 27", doc.slot27)
+                            .addField("Slot 28", doc.slot28)
+                            .addField("Slot 29", doc.slot29)
+                            .addField("Slot 30", doc.slot30)
+                            .addField("Slot 31", doc.slot31)
+                            .addField("Slot 32", doc.slot32)
+                            .addField("Head", doc.head)
+                            .addField("Chest", doc.chest)
+                            .addField("Torse", doc.torso)
+                            .addField("Shoes", doc.shoe)
+                        
                         .setFooter(`• Requested By: ${message.author.tag}`, message.author.avatarURL)
                         message.author.send(inventoryembed1);
                         message.author.send(invEmbed2);
